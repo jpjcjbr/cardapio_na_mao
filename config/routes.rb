78241f09f63e55dd0128b41441f1cc984c963b55
JP <jpjcjbr@gmail.com>
2011-04-03 01:15:02 -0300

    Exibicao de imagens na edicao

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CardapioNaMao::Application.routes.draw do
 	match 'categorias_json' => 'categorias#all_categorias_from_user'
-	match 'itens_json' => 'itens#all_itens_from_user'
+	match 'itens_json' => 'itens#all_itens_from_user'	
 	match 'download' => redirect('/CardapioNaMao.apk')	
 	match 'home' => 'pagina_inicial#index'
 	match 'ajuda' => 'pagina_inicial#ajuda'
