e442e4faa4945736c91fec32610404916c26d942
JP <jpjcjbr@gmail.com>
2011-04-05 21:41:13 -0300

    Upload de imagens no IE corrigido

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ class Item < ActiveRecord::Base
 	has_attached_file :imagem, 
 		:storage => :s3,
 		:s3_credentials => Rails.root.join('config/s3.yml'),
-		:path => "/:style/:filename",
+		:path => "/:style/:id",
 		:styles => { :medium => "300x300>", :thumb => "100x100#", :original => "800x600>" }
 			
-	validates_attachment_content_type :imagem, :content_type => [ 'image/jpeg', 'image/gif', 'image/png', 'image/bmp', 'image/jpg' ]
+	#validates_attachment_content_type :imagem, :content_type => [ 'image/jpeg', 'image/gif', 'image/png', 'image/bmp', 'image/jpg' ]
 	validates_attachment_size :imagem, :less_than => 500.kilobytes		
 	
 	def url_imagem
