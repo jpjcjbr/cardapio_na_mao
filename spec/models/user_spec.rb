fad840a284b4dda04554496258628afbc8b2d00b
João Paulo <jpjcjbr@gmail.com>
2011-05-01 09:55:58 -0300

    Novo teste da factory de usuarios

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,10 @@ describe User do
   it "deve possuir uma senha" do
     User.new.should respond_to(:password)
   end
+  
+  it "pode gerar um usuario usando a factory" do
+    item = Factory :user
+    item.should_not be_nil
+    item.should be_kind_of(User)
+  end
 end
